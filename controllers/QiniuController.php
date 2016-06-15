69eb7e48b4b08a8ae292fedc91efe8b219b7baac
evan.bai <cn.baiying@gmail.com>
2016-06-15 16:27:07 +0800

    添加七牛云上传图片demo程序

---
+++ controllers/QiniuController.php
@@ -0,0 +1,30 @@
+<?php
+namespace app\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+class QiniuController extends Controller {
+    
+    public function actionIndex() {
+        
+        return $this->render('index');
+    }
+    
+    public function actionAjax() {
+        if(!Yii::$app->request->get('act')) {
+            $json = ['status'=>'fail', 'message'=>'缺少act参数'];
+            exit(Json::encode($json));
+        }
+        $act = Yii::$app->request->get('act');
+        switch($act) {
+            // 请求上传token
+            case 'token':
+                $res = Yii::$app->api->get('qiniu/get-token');
+                exit(json_encode(['uptoken'=>$res['data']]));
+                break;
+        }
+        
+        
+    }
+}
