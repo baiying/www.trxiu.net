e2e3b71374edf566dfc92955a4975827992abb29
turenquan <turenqian1@163.com>
2016-07-02 10:15:51 +0800

    修复授权返回参数问题

--- web/static/js/libs/login.js
+++ web/static/js/libs/login.js
@@ -6,9 +6,12 @@ function($) {
             var $this = this;
             if ( !! $this.getCookie("DA7F9084C361196F1147D0DE68FEC172")) {
                 var userInfo = $this.getCookie("DA7F9084C361196F1147D0DE68FEC172");
+                userInfo=decodeURIComponent(userInfo);
                 callback($.parseJSON(userInfo));
             } else {
                 var REDIRECT_URI = encodeURIComponent(location.href);
+
+                //wx0a1799c10d53e3c0
                 var openurl = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxcbfa7f22b7956568&redirect_uri=" + REDIRECT_URI + "&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect";
 
                 if ($this.GetURLParameter('code')) {
@@ -25,7 +28,7 @@ function($) {
                             //alert(JSON.stringify(resp));
                             if(resp.status=="success"){
                                 var value=JSON.stringify(resp.data);
-                                $this.setCookie("DA7F9084C361196F1147D0DE68FEC172", value, 30);
+                                $this.setCookie("DA7F9084C361196F1147D0DE68FEC172", encodeURIComponent(value), 30);
                                 callback(resp.data);
                             }
                             else{
