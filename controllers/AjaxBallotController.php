679862b996c2adcb006c11141358ef593bd0a3ed
Maple <crazy.maple@foxmail.com>
2016-07-05 12:28:29 +0800

    修改分享头像 分享标题 分享描述

--- controllers/AjaxBallotController.php
+++ controllers/AjaxBallotController.php
@@ -94,7 +94,7 @@ class AjaxBallotController extends AjaxBaseController {
         $args = $this->getRequestData($rule, Yii::$app->request->get());
         $res = Yii::$app->api->get('ballot/anchor-in-ballot', $args);
         if($res['code'] == 200) {
-            $res['data']['ShareTile'] = "小伙伴们快来帮我投个票哈";
+            $res['data']['ShareTile'] = "萌主派对第一季，闪亮主播风云榜";
             $res['data']['ShareDescripion'] = "参与萌主派对，帮你的#".$res['data']['name']."#投票赢手机！";
             $res['data']['ShareImg'] = $res['data']['thumb'];
             $this->export('success', $res['message'], $res['data']);
